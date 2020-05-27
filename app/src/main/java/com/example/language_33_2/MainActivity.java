7ab0f4d3aba6a1b04a1b6c608e438a54d802ee49
ki1n <turevno@gmail.com>
2020-05-27 11:50:00 +0600

    Android Styles Margin 3.4.2

--- app/src/main/java/com/example/language_33_2/MainActivity.java
+++ app/src/main/java/com/example/language_33_2/MainActivity.java
@@ -12,17 +12,19 @@ import android.widget.Spinner;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private Spinner mSpinnerLang, mSpinnerColors;
+    private Spinner mSpinnerLang, mSpinnerColors, mSpinnerMarginLay;
     private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetThemeMl(this);
         Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_main);
 
         mSpinnerLang = findViewById(R.id.spinner);
         mSpinnerColors = findViewById(R.id.spinnerColors);
+        mSpinnerMarginLay = findViewById(R.id.spinnerMarginLay);
         mButton = findViewById(R.id.button);
 
         initSpinnerCountries();
@@ -31,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 int selected = mSpinnerLang.getSelectedItemPosition();
-                switch (selected){
+                switch (selected) {
                     case 0:
                         switchLocale("en");
                         break;
@@ -40,15 +42,28 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 int selectColor = mSpinnerColors.getSelectedItemPosition();
-                switch (selectColor){
+                switch (selectColor) {
                     case 0:
-                        Utils.changeToTheme(MainActivity.this,Utils.THEME_BLACK);
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLACK);
                         break;
                     case 1:
-                        Utils.changeToTheme(MainActivity.this,Utils.THEME_GREEN);
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_GREEN);
                         break;
                     case 2:
-                        Utils.changeToTheme(MainActivity.this,Utils.THEME_BLUE);
+                        Utils.changeToTheme(MainActivity.this, Utils.THEME_BLUE);
+                        break;
+                }
+
+                int selectMargin = mSpinnerMarginLay.getSelectedItemPosition();
+                switch (selectMargin) {
+                    case 0:
+                        Utils.changeMlToTheme(MainActivity.this, Utils.THEME_MARGIN_1);
+                        break;
+                    case 1:
+                        Utils.changeMlToTheme(MainActivity.this, Utils.THEME_MARGIN_3);
+                        break;
+                    case 2:
+                        Utils.changeMlToTheme(MainActivity.this, Utils.THEME_MARGIN_10);
                         break;
                 }
             }
@@ -63,6 +78,11 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.сolorsArr, android.R.layout.simple_spinner_item);
         mSpinnerColors.setAdapter(adapter);
+
+        ArrayAdapter<CharSequence> adapterMargin = ArrayAdapter.createFromResource(this,
+                R.array.marginLayoutArr, android.R.layout.simple_spinner_item);
+        mSpinnerMarginLay.setAdapter(adapterMargin);
+
     }
 
     private void switchLocale(String language) {
--- app/src/main/java/com/example/language_33_2/Utils.java
+++ app/src/main/java/com/example/language_33_2/Utils.java
@@ -4,16 +4,42 @@ import android.app.Activity;
 import android.content.Intent;
 
 public class Utils {
-    private static int sTheme;
+    private static int sTheme;  // для цвета
+    private static int mlTheme; // для отступа
 
     public final static int THEME_BLACK = 0;
     public final static int THEME_GREEN = 1;
     public final static int THEME_BLUE = 2;
 
+    public final static int THEME_MARGIN_1 = 0;
+    public final static int THEME_MARGIN_3 = 1;
+    public final static int THEME_MARGIN_10 = 2;
+
     public static void changeToTheme(Activity activity, int theme) {
         sTheme = theme;
         activity.finish();
         activity.startActivity(new Intent(activity, activity.getClass()));
+    }
+
+    public static void changeMlToTheme(Activity activity, int theme) {
+        mlTheme = theme;
+        activity.finish();
+        activity.startActivity(new Intent(activity, activity.getClass()));
+    }
+
+    public static void onActivityCreateSetThemeMl(Activity activity) {
+        switch (mlTheme) {
+            default:
+            case THEME_MARGIN_1:
+                activity.setTheme(R.style.Margin_1);
+                break;
+            case THEME_MARGIN_3:
+                activity.setTheme(R.style.Margin_3);
+                break;
+            case THEME_MARGIN_10:
+                activity.setTheme(R.style.Margin_10);
+                break;
+        }
     }
 
     public static void onActivityCreateSetTheme(Activity activity) {
